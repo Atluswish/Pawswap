adb6ce378a9b6b14a0418048f5771246eb3acc62
Atluswish <lennonwoods3004@gmail.com>
2025-04-15 00:41:59 +0200

    Created custom map icon

--- pawswapmap.js
+++ pawswapmap.js
@@ -1,9 +1,20 @@
 // JavaScript Document
 
-var map = L.map('map').setView([51.505, -0.09], 13);
 
+
+    var map = L.map('map').setView([51.5898, 4.7754], 13);
+
+var pawIcon = L.icon({
+    iconUrl: 'images/orangepinswapmap.png',  
+    iconSize: [40, 40],              
+    iconAnchor: [20, 40],            
+    popupAnchor: [0, -40]
+})
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+L.marker([51.60281571895569, 4.7816432910496225], { icon: pawIcon }).addTo(map)
+  .bindPopup("You clicked here!");
