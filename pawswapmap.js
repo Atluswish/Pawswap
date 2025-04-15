e2258be3da8b58939ef4e7a3ccc739637381c3b4
Atluswish <lennonwoods3004@gmail.com>
2025-04-15 21:28:18 +0200

    Made the map move to the home page
    Additionally the map popup was altered to be within our brands looks and given a direct link to the Products page :)

--- pawswapmap.js
+++ pawswapmap.js
@@ -6,9 +6,9 @@
 
 var pawIcon = L.icon({
     iconUrl: 'images/orangepinswapmap.png',  
-    iconSize: [40, 40],              
-    iconAnchor: [20, 40],            
-    popupAnchor: [0, -40]
+    iconSize: [80, 80],              
+    iconAnchor: [40, 80],            
+    popupAnchor: [0, -80]
 })
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -16,5 +16,30 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+//Swap Marker 1
 L.marker([51.60281571895569, 4.7816432910496225], { icon: pawIcon }).addTo(map)
-  .bindPopup("You clicked here!");
+  .bindPopup('<strong>Bark Enjoyers<strong><br>Members: 10<br>Swaps:23<br><a href="dogproducts.html" target="Blank">Swaps Available</a>');
+			 
+
+var circle = L.circle([51.60281571895569, 4.7816432910496225], {
+    color: 'orange',
+    fillColor: '#ffb342',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+//Swap Marker 2
+L.marker([51.586768, 4.74336], { icon: pawIcon }).addTo(map)
+ .bindPopup("The Hond lovers<br>Members: 12<br>Swaps:31")
+
+
+//Remove the below after use as it is only for finding coordinates on the map for Pins
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
